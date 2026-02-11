693e607f063cb317237328c61cdb91fae1cb27bd
EmilAlexLarsson <emilalexlarsson@gmail.com>
2026-02-11 14:06:32 +0100

    ...

--- ChalkboardChat.BLL/DTOs/MessageDtos/CreateMessageDto.cs
+++ ChalkboardChat.BLL/DTOs/MessageDtos/CreateMessageDto.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace ChalkboardChat.BLL.DTOs.MessageDtos
 {
-    internal class CreateMessageDto
+    public class CreateMessageDto
     {
         //public int Id { get; set; } behövs inte för att databasen skapar det åt oss
         public DateTime Date { get; set; }
--- ChalkboardChat.BLL/Services/MessageService.cs
+++ ChalkboardChat.BLL/Services/MessageService.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using ChalkboardChat.BLL.DTOs.MessageDtos;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ChalkboardChat.BLL.Services
 {
-    internal class MessageService
+    public class MessageService
     {
+        private readonly AppDbContext _context;
+
+        public MessageService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<MessageModel>> GetAllMessagesAsync()
+        {
+            return await _context.Messages.ToListAsync();
+
+
+        }
     }
 }
