90c5934bf9dcd4eaf10f72dfa19597f6c6f86bcf
Filippa24 <filippa98b@hotmail.se>
2026-02-11 12:07:06 +0100

    gjort dtos

---
+++ ChalkboardChat.BLL/DTOs/MessageDtos/CreateMessageDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ChalkboardChat.BLL.DTOs.MessageDtos
+{
+    internal class CreateMessageDto
+    {
+        //public int Id { get; set; } behövs inte för att databasen skapar det åt oss
+        public DateTime Date { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+    }
+}
