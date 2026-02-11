6bd8b9fccecb972c18671ed4086c17c39b250bda
EmilAlexLarsson <emilalexlarsson@gmail.com>
2026-02-11 14:57:28 +0100

    message klar???

--- ChalkboardChat.BLL/Services/MessageService.cs
+++ ChalkboardChat.BLL/Services/MessageService.cs
@@ -1,11 +1,12 @@
 ﻿using ChalkboardChat.BLL.DTOs.MessageDtos;
+using ChalkboardChat.BLL.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ChalkboardChat.BLL.Services
 {
-    public class MessageService
+    public class MessageService : IMessageService
     {
         private readonly AppDbContext _context;
 
@@ -19,12 +20,31 @@ namespace ChalkboardChat.BLL.Services
             return await _context.Messages.OrderByDescending(m => m.Date)
                 .Select(m => new MessageListDto
                 {
+                    Id = m.Id,
                     Date = m.Date,
                     Message = m.Message,
                     Username = m.Username
                 })
                 .ToListAsync();
 
+        }
+        public async Task<MessageDetailDto> CreateMessageAsync(CreateMessageDto dto)
+        {
+            var newMessage = new Message
+            {
+                Date = dto.Date,
+                Message = dto.Message,
+                Username = dto.Username
+            };
+            await _context.Messages.AddAsync(newMessage);
+            await _context.SaveChangesAsync();
+
+            return new MessageDetailDto
+            {
+                Date = newMessage.Date,
+                Message = newMessage.Message,
+                Username = newMessage.Username
+            };
         }
     }
 }
