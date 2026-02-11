5e1ccafeafceffec69c42c78c80d9d7177552eca
Jontiq <99jonathancar@gmail.com>
2026-02-11 14:41:29 +0100

    Added Databases

---
+++ ChalkboardChat.DAL/DATAs/MessageDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ChalkboardChat.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChalkboardChat.DAL.DATAs
+{
+    public class MessageDbContext : DbContext
+    {
+        public MessageDbContext(DbContextOptions<MessageDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<MessageModel> Messages => Set<MessageModel>();
+    }
+}
