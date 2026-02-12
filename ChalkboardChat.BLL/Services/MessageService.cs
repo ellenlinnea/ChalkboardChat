8311fd25b08cbc2ad6d9ccbc69c426dbeccb0659
EmilAlexLarsson <emilalexlarsson@gmail.com>
2026-02-12 12:42:37 +0100

    ändringar

--- ChalkboardChat.BLL/Services/MessageService.cs
+++ ChalkboardChat.BLL/Services/MessageService.cs
@@ -1,6 +1,7 @@
 ﻿using ChalkboardChat.BLL.DTOs.MessageDtos;
 using ChalkboardChat.BLL.Interfaces;
 using ChalkboardChat.DAL.DATAs;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -10,44 +11,68 @@ namespace ChalkboardChat.BLL.Services
 {
     public class MessageService : IMessageService
     {
-        private readonly MessageDbContext _context;
+        private readonly MessageRepository _messageRepo;
         private readonly AuthDbContext _authContext;
-        public MessageService(MessageDbContext context, AuthDbContext authContext)
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public MessageService(MessageRepository messageRepository, AuthDbContext authContext, SignInManager<IdentityUser> signInManager)
         {
-            _context = context;
+            _messageRepo = messageRepository;
             _authContext = authContext;
-        }
-
-        public async Task<List<MessageListDto>> GetAllMessagesAsync()
-        {
-            return await _context.Messages.OrderByDescending(m => m.Date)
-                .Select(m => new MessageListDto
-                {
-                    Id = m.Id,
-                    Date = m.Date,
-                    Message = m.Message,
-                    Username = m.Username
-                })
-                .ToListAsync();
+            _signInManager = signInManager;
 
         }
-        public async Task<MessageDetailDto> CreateMessageAsync(CreateMessageDto dto)
-        {
-            var newMessage = new DAL.Models.MessageModel
-            {
-                Date = dto.Date,
-                Message = dto.Message,
-                Username = dto.Username
-            };
-            await _context.Messages.AddAsync(newMessage);
-            await _context.SaveChangesAsync();
 
-            return new MessageDetailDto
+        public Task<List<MessageListDto>> GetAllMessages()
+        {
+            //return await _messageRepo.Messages.OrderByDescending(m => m.Date)
+            //    .Select(m => new MessageListDto
+            //    {
+            //        Id = m.Id,
+            //        Date = m.Date,
+            //        Message = m.Message,
+            //        Username = m.Username
+            //    })
+            //    .ToListAsync();
+
+            var messages = _messageRepo.GetAllMessagesAsync();
+            messages = new MessageListDto
             {
-                Date = newMessage.Date,
-                Message = newMessage.Message,
-                Username = newMessage.Username
+                Id = messages.Id,
+                Date = messages.Date,
+                Message = messages.Message,
+                Username = messages.Username
             };
+            if (messages == null)
+            {
+                throw new Exception("No messages found.");
+            }
+            return messages;
+
+        }
+        public Task<MessageDetailDto> CreateMessage(string message)
+        {
+            var username = 
+
+            var newMessage = _messageRepo.CreateMessageAsync(username);
+            
+
+
+            //var newMessage = new DAL.Models.MessageModel
+            //{
+            //    Date = dto.Date,
+            //    Message = dto.Message,
+            //    Username = dto.Username
+            //};
+            //await _context.Messages.AddAsync(newMessage);
+            //await _context.SaveChangesAsync();
+
+            //return new MessageDetailDto
+            //{
+            //    Date = newMessage.Date,
+            //    Message = newMessage.Message,
+            //    Username = newMessage.Username
+            //};
         }
     }
 }
